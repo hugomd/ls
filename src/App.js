47f6646daceedd90c09a0902a5583c9e8e2d6776
Hugo <h@hugo.sx>
2017-03-12 20:49:01 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Footer, Heading, PageHeader } from 'rebass';
+import init from './basicTheme';
 
 class App extends Component {
   render() {
+    const {
+       fontFamily,
+       fontWeight,
+       letterSpacing,
+       color,
+       backgroundColor,
+       drawerOpen,
+       overlayOpen
+    } = init;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{
+        fontFamily,
+        fontWeight,
+        letterSpacing,
+        color,
+        backgroundColor
+        }}>
+        <Container>
+          <PageHeader
+            description="A list of microservices I've published with now."
+            heading='ls' />
+          <a href='https://f.now.sh/'>micro-flags</a> &mdash; a microservice that returns a flag for a given country code.<br />
+            <a href='https://e.now.sh/'>micro-emoji</a> &mdash; a microservice that returns an emoji for a given emoji shortname.<br />
+            <a href='https://up.now.sh/'>micro-up</a> &mdash; a microservice that returns based on whether a website is up or not.<br />
+          <Footer>
+            Made with &hearts; in Melbourne, by&nbsp;<a href='https://hugo.md/'>Hugo</a>.
+          </Footer>
+        </Container>
       </div>
     );
   }
